ceb9d614a32feed414f5827563dd8813e661aba9
hemanthlucifer <108113190+hemanthlucifer@users.noreply.github.com>
2024-01-08 11:17:04 +0530

    initial commit
    initial commit for logistics service

---
+++ src/test/java/com/inventoryApplication/logisticsService/LogisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventoryApplication.logisticsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
