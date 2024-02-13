894acc4244f7a94b14a47251bb05c41341648877
hemanthlucifer <108113190+hemanthlucifer@users.noreply.github.com>
2024-02-13 15:20:02 +0530

    initial commit

--- src/test/java/com/application/warehouseManagement/WarehouseManagementApplicationTests.java
+++ src/test/java/com/application/warehouseManagement/WarehouseManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.inventoryApplication.logisticsService;
+package com.application.warehouseManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LogisticsServiceApplicationTests {
+class WarehouseManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
